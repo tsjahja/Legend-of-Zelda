e1423b53e2c0fbaf87a2d74edf489a818504f6c4
Toshiki Sutisna Jahja <tsjahja@umich.edu>
2015-01-21 19:43:05 -0500

    Add background and stalfos enemies

--- alpha/Assets/stalfos.cs
+++ alpha/Assets/stalfos.cs
@@ -4,17 +4,30 @@ using System.Collections;
 public class stalfos : MonoBehaviour {
 
 	public GameObject enemy;
-	public float speed = 1f;
+	private float speed = 0.25f;
+	private bool direction = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 pos = transform.position;
-		pos.x += speed * Time.deltaTime;
+		if (direction) pos.x += speed * Time.deltaTime;
+		else pos.y += speed * Time.deltaTime;
 		transform.position = pos;
+
+	}
+
+	void FixedUpdate() {
+
+		if (Random.value < 0.01f && Random.value >= 0.005f) {
+			speed *= -1;
+		} else if (Random.value < 0.005f) {
+			if(direction) direction = false;
+			else direction = true;
+		}
 	}
 }
